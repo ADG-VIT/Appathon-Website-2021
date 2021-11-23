b5a46812ec8d0173124e98be66a5bdc98e2aeee1
saswatabasu23 <saswatabasu23@gmail.com>
2021-11-23 18:15:15 +0530

    top button added

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ function updateTimer() {
 	timer.innerHTML = "0" + d + ":" + h + ":" + m + ":" + s;
 }
 setInterval("updateTimer()", 1000);
+
+
+mybutton = document.getElementById("f-btn");
+
+window.onscroll = function () {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
