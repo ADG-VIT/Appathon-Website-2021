81b0e6aaf1aa51a7026767b873750f339434c607
anmolbansal7 <abanmolbansal5@gmail.com>
2021-11-23 18:20:29 +0530

    minor update

--- script.js
+++ script.js
@@ -34,3 +34,26 @@ function sponsors() {
 	document.getElementById("sponsors").style.display = "block";
 	document.getElementById("speakers").style.display = "none";
 }
+
+//Footer
+mybutton = document.getElementById("f-btn");
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (
+		document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20
+	) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
